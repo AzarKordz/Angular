c7c83f394c6af3c0eeab3dad43a3fd66ef0d6232
Mohammed Azarudeen J <mohameedazarudeen.j@ivygdcad.ivy-support.com>
2022-06-26 17:54:43 +0530

    uploaded task

---
+++ AngularJs/Scripts/Script.js
@@ -0,0 +1,412 @@
+﻿
+/*var myApp = angular.module("myModule", [])*/
+
+/*var myController = function ($scope) {
+    $scope.message = "AngularJS Tutorial";
+}*/
+/*----------------------------------------------------------------------------*/
+/*var myApp = angular
+    .module("myModule", [])
+    .controller("myController", function ($scope) {
+        var employee = {
+            firstName: 'Mark',
+            lastName: 'Hastings',
+            gender: 'Male'
+        };
+        $scope.employee = employee;
+    }); */
+/*----------------------------------------------------------------------------*/
+/*var myApp = angular
+    .module("myModule", [])
+    .controller("myController", function ($scope) {
+        var country = {
+            name: "IVY",
+            capital: "Chennai",
+            flag: "/Images/Ivy-Logo-1.png"
+        };
+        $scope.country = country;
+    });*/
+/*----------------------------------------------------------------------------*/
+/*var myApp = angular
+    .module("myModule", [])
+    .controller("myController", function ($scope) {
+        var country = {
+            name: "IVY",
+            city: "Chennai",
+            country : "India"
+        };
+        $scope.country = country
+        $scope.message = "Hello IVY"
+    });
+    */
+/*----------------------------------------------------------------------------*/
+
+/*var app = angular
+    .module("myModule", [])
+    .controller("myController", function ($scope) {
+       /* var employees = [
+            { firstName: "Mohammed", lastName: "Azarudeen", gender: "Male", salary: 50000 },
+            { firstName: "Naveen", lastName: "Kumar", gender: "Male", salary: 50000 },
+            { firstName: "Dhusanthan", lastName: "Rajendhiran", gender: "Male", salary: 60000 },
+            { firstName: "Nandha", lastName: "Kumar", gender: "Male", salary: 70000 },
+            { firstName: "Mohammed", lastName: "Azar", gender: "Male", salary: 50000 }
+        ];
+        $scope.employees = employees
+        $scope.message = "Hello IVY"*/
+       /* var countries = [
+            {
+                name: "India",
+                citiies: [
+                    { name: "Chennai" },
+                    { name: "Bangalore" },
+                    { name: "Hyderabad" }
+                ]
+            },
+            {
+                name: "UK",
+                citiies: [
+                    { name: "London" },
+                    { name: "Manchestor" },
+                    { name: "Birmingham" }
+                ]
+            },
+            {
+                name: "USA",
+                citiies: [
+                    { name: "Los Angels" },
+                    { name: "Chicago" },
+                    { name: "Houstan" }
+                ]
+            }
+        ];*/
+
+        
+
+//var app = angular
+//    .module("myModule", [])
+//    .controller("myController", function ($scope) {
+
+//        var countries = [
+//            {
+//                name: "UK",
+//                cities: [
+//                    { name: "London" },
+//                    { name: "Birmingham" },
+//                    { name: "Manchester" }
+//                ]
+//            },
+//            {
+//                name: "USA",
+//                cities: [
+//                    { name: "Los Angeles" },
+//                    { name: "Chicago" },
+//                    { name: "Houston" }
+//                ]
+//            },
+//            {
+//                name: "India",
+//                cities: [
+//                    { name: "Hyderabad" },
+//                    { name: "Chennai" },
+//                    { name: "Mumbai" }
+//                ]
+//            }
+//        ];
+
+//        $scope.countries = countries;
+//    });
+
+
+//-----------------------------------------------------------------------------------------
+
+//var app = angular
+//    .module("myModule", [])
+//    .controller("myController", function ($scope) {
+
+//        var technologies = [
+//            { name: "C#", likes: 0, dislikes: 0 },
+//            { name: "ASP.NET", likes: 0, dislikes: 0 },
+//            { name: "SQL", likes: 0, dislikes: 0 },
+//            { name: "AngularJS", likes: 0, dislikes: 0 }
+//        ];
+
+//        $scope.technologies = technologies;
+
+//        $scope.incrementLikes = function (technology) {
+//            technology.likes++;
+//        };
+
+//        $scope.incrementDislikes = function (technology) {
+//            technology.dislikes++;
+//        };
+//    });
+
+//-----------------------------------------------------------------------------------------
+
+//var app = angular
+//    .module("myModule", [])
+//    .controller("myController", function ($scope) {
+
+//        var employees = [
+//            {
+//                name: "Ben", dateOfBirth: new Date("November 23, 1980"),
+//                gender: "Male", salary: 55000.788
+//            },
+//            {
+//                name: "Sara", dateOfBirth: new Date("May 05, 1970"),
+//                gender: "Female", salary: 68000
+//            },
+//            {
+//                name: "Mark", dateOfBirth: new Date("August 15, 1974"),
+//                gender: "Male", salary: 57000
+//            },
+//            {
+//                name: "Pam", dateOfBirth: new Date("October 27, 1979"),
+//                gender: "Female", salary: 53000
+//            },
+//            {
+//                name: "Todd", dateOfBirth: new Date("December 30, 1983"),
+//                gender: "Male", salary: 60000
+//            }
+//        ];
+
+//        $scope.employees = employees;
+//        $scope.rowCount = 3;
+//    });
+
+//-----------------------------------------------------------------------------------------
+
+//var app = angular
+//    .module("myModule", [])
+//    .controller("myController", function ($scope) {
+
+//        var employees = [
+//            {
+//                name: "Ben", dateOfBirth: new Date("November 23, 1980"),
+//                gender: "Male", salary: 55000
+//            },
+//            {
+//                name: "Sara", dateOfBirth: new Date("May 05, 1970"),
+//                gender: "Female", salary: 68000
+//            },
+//            {
+//                name: "Mark", dateOfBirth: new Date("August 15, 1974"),
+//                gender: "Male", salary: 57000
+//            },
+//            {
+//                name: "Pam", dateOfBirth: new Date("October 27, 1979"),
+//                gender: "Female", salary: 53000
+//            },
+//            {
+//                name: "Todd", dateOfBirth: new Date("December 30, 1983"),
+//                gender: "Male", salary: 60000
+//            }
+//        ];
+
+//        $scope.employees = employees;
+//        $scope.sortColumn = "name";
+
+//    });
+
+//-----------------------------------------------------------------------------------------
+
+//var app = angular
+//    .module("myModule", [])
+//    .controller("myController", function ($scope) {
+
+//        var employees = [
+//            {
+//                name: "Ben", dateOfBirth: new Date("November 23, 1980"),
+//                gender: "Male", salary: 55000
+//            },
+//            {
+//                name: "Sara", dateOfBirth: new Date("May 05, 1970"),
+//                gender: "Female", salary: 68000
+//            },
+//            {
+//                name: "Mark", dateOfBirth: new Date("August 15, 1974"),
+//                gender: "Male", salary: 57000
+//            },
+//            {
+//                name: "Pam", dateOfBirth: new Date("October 27, 1979"),
+//                gender: "Female", salary: 53000
+//            },
+//            {
+//                name: "Todd", dateOfBirth: new Date("December 30, 1983"),
+//                gender: "Male", salary: 60000
+//            }
+//        ];
+
+//        $scope.employees = employees;
+//        $scope.sortColumn = "name";
+//        $scope.reverseSort = false;
+
+//        $scope.sortData = function (column) {
+//            $scope.reverseSort = ($scope.sortColumn == column) ?
+//                !$scope.reverseSort : false;
+//            $scope.sortColumn = column;
+//        }
+
+//        $scope.getSortClass = function (column) {
+
+//            if ($scope.sortColumn == column) {
+//                return $scope.reverseSort
+//                    ? 'arrow-down'
+//                    : 'arrow-up';
+//            }
+
+//            return '';
+//        }
+//    });
+
+//----------------------------------------------------------------------------------------------------
+
+//var app = angular
+//    .module("myModule", [])
+//    .controller("myController", function ($scope) {
+
+//        var employees = [
+//            { name: "Ben", gender: "Male", salary: 55000, city: "London" },
+//            { name: "Sara", gender: "Female", salary: 68000, city: "Chennai" },
+
+
+//            { name: "Mark", gender: "Male", salary: 57000, city: "London" },
+
+
+//            { name: "Pam", gender: "Female", salary: 53000, city: "Chennai" },
+//            { name: "Todd", gender: "Male", salary: 60000, city: "London" },
+//        ];
+
+//        $scope.employees = employees;
+//    });
+//--------------------------------------------------------------------------------------------------------------------------------------------------------
+//var app = angular
+//    .module("myModule", [])
+//    .controller("myController", function ($scope) {
+
+//        var employees = [
+//            { name: "Ben", gender: "Male", salary: 55000, city: "London" },
+//            { name: "Sara", gender: "Female", salary: 68000, city: "Chennai" },
+//            { name: "Mark", gender: "Male", salary: 57000, city: "London" },
+//            { name: "Pam", gender: "Female", salary: 53000, city: "Chennai" },
+
+
+//            { name: "Todd", gender: "Male", salary: 60000, city: "London" },
+//        ];
+
+//        $scope.employees = employees;
+//    });
+
+//var app = angular
+//    .module("myModule", [])
+//    .controller("myController", function ($scope) {
+
+//        var employees = [
+//            { name: "Ben", gender: "Male", salary: 55000, city: "London" },
+//            { name: "Sara", gender: "Female", salary: 68000, city: "Chennai" },
+
+
+//            { name: "Mark", gender: "Male", salary: 57000, city: "London" },
+
+
+//            { name: "Pam", gender: "Female", salary: 53000, city: "Chennai" },
+//            { name: "Todd", gender: "Male", salary: 60000, city: "London" },
+//        ];
+
+//        $scope.employees = employees;
+
+//        $scope.search = function (item) {
+//            if ($scope.searchText == undefined) {
+//                return true;
+//            }
+//            else {
+//                if (item.city.toLowerCase()
+//                    .indexOf($scope.searchText.toLowerCase()) != -1 ||
+//                    item.name.toLowerCase()
+//                        .indexOf($scope.searchText.toLowerCase()) != -1) {
+//                    return true;
+//                }
+//            }
+
+//            return false;
+//        };
+//    });
+
+//-------------------------------------------------------------------------------------------------------------
+
+//var app = angular
+//    .module("myModule", [])
+//    .controller("myController", function ($scope) {
+
+//        var employees = [
+//            { name: "Ben", gender: 1, salary: 55000 },
+//            { name: "Sara", gender: 2, salary: 68000 },
+//            { name: "Mark", gender: 1, salary: 57000 },
+//            { name: "Pam", gender: 2, salary: 53000 },
+//            { name: "Todd", gender: 3, salary: 60000 }
+//        ];
+
+//        $scope.employees = employees;
+//    });
+
+//-------------------------------------------------------------------------------------------------------------
+
+
+//var app = angular
+//    .module("myModule", [])
+//    .controller("myController", function ($scope) {
+
+//        var employees = [
+//            { name: "Ben", gender: "Male", city: "London", salary: 55000 },
+//            { name: "Sara", gender: "Female", city: "Chennai", salary: 68000 },
+
+
+//            { name: "Mark", gender: "Male", city: "Chicago", salary: 57000 },
+
+
+//            { name: "Pam", gender: "Female", city: "London", salary: 53000 },
+//            { name: "Todd", gender: "Male", city: "Chennai", salary: 60000 }
+//        ];
+
+//        $scope.employees = employees;
+//    });
+
+//var app = angular
+//    .module("myModule", [])
+//    .controller("myController", function ($scope) {
+//        var countries = [
+//            {
+//                name: "India",
+//                cities: [
+//                    { name: "Hyderabad" },
+//                    { name: "Chennai" }
+//                ]
+//            },
+//            {
+//                name: "USA",
+//                cities: [
+//                    { name: "Los Angeles" },
+//                    { name: "Chicago" },
+//                ]
+//            }
+//        ];
+
+//        $scope.countries = countries;
+//    });
+//----------------------------------------------------------------------------------------
+var app = angular
+    .module("myModule", [])
+    .controller("myController", function ($scope) {
+
+        var employees = [
+            { name: "Ben", gender: "Male", salary: 55000 },
+            { name: "Sara", gender: "Female", salary: 68000 },
+            { name: "Mark", gender: "Male", salary: 57000 },
+            { name: "Pam", gender: "Female", salary: 53000 },
+            { name: "Todd", gender: "Male", salary: 60000 }
+        ];
+
+        $scope.employees = employees;
+        $scope.employeeView = "EmployeeTable.html";
+    });
+
